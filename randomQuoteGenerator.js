fa7a30e8798b52c4247dda5f13f770a5365bc3da
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-13 01:27:25 +0800

    modify file

--- randomQuoteGenerator.js
+++ randomQuoteGenerator.js
@@ -1,35 +1,11 @@
 $(document).ready(function() {
-    loadData();
-    var newQuote;
-    var newAuthor;
-
-    function loadData() {
-        //Make an AJAX Request
-        $.ajax({
-            url: "http://api.forismatic.com/api/1.0/",
-            jsonp: "jsonp",
-            dataType: "jsonp",
-            data: {
-                method: "getQuote",
-                lang: "en",
-                format: "jsonp"
-            },
-            success: function(res) {
-                newQuote = res.quoteText;
-                newAuthor = res.quoteAuthor;
-                $("#quote-box").html(newQuote);
-                if(newAuthor) {
-                    $("#author").html("- " + newAuthor)
-                } else {
-                    $("#author").html("- " + "Anonymous")
-                }     
+    $('#quote-box').on('click'.function() {
+        $ajax({
+            url: "/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+            dataType: 'json',
+            success: function() {
             }
-        });
-    }
-
-    $("button").click(loadData);
-
-       
-
-
+        }
+    })
+    
 })
