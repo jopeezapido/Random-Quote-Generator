72f26862841e22ef98e9e1ea107d7c1f22e8a2c9
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-12 21:29:24 +0800

    rename file

---
+++ randomQuoteGenerator.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    loadData();
+    var newQuote;
+    var newAuthor;
+
+    function loadData() {
+        //Make an AJAX Request
+        $.ajax({
+            url: "http://api.forismatic.com/api/1.0/",
+            jsonp: "jsonp",
+            dataType: "jsonp",
+            data: {
+                method: "getQuote",
+                lang: "en",
+                format: "jsonp"
+            },
+            success: function(res) {
+                newQuote = res.quoteText;
+                newAuthor = res.quoteAuthor;
+                $("#quote-box").html(newQuote);
+                if(newAuthor) {
+                    $("#author").html("- " + newAuthor)
+                } else {
+                    $("#author").html("- " + "Anonymous")
+                }     
+            }
+        });
+    }
+
+    $("button").click(loadData);
+
+    
+
+
+})
