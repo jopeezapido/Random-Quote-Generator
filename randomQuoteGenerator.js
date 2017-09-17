c35c41a6405f6ba97500f89bcef6b49fc8aafd47
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-17 13:40:35 +0800

    returning back to fix github pages issue

--- randomQuoteGenerator.js
+++ randomQuoteGenerator.js
@@ -12,29 +12,6 @@ $(document).ready(function() {
 
         var url = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
 
-        //Make an AJAX Request - Old version (did not work because of redundant json and url format indications)
-
-        /*$.ajax({
-            url: url,
-            json: "json",
-            dataType: "json",
-            data: {
-                method: "getQuote",
-                lang: "en",
-                format: "json"
-            },
-            success: function(res) {
-                newQuote = res.quoteText;
-                newAuthor = res.quoteAuthor;
-                $("#quote-box").html('"' + newQuote + '"');
-                if(newAuthor) {
-                    $("#author").html("- " + newAuthor)
-                } else {
-                    $("#author").html("- " + "Anonymous")
-                }
-            }
-        });*/
-
         //new JSON request
 
         $get.JSON(url, function(res){
