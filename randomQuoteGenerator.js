ee6f779c1c4c287b3e704c24f4c33d9dd3e53654
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-17 12:06:25 +0800

    comment out old ajax request and replace with get.JSON request

--- randomQuoteGenerator.js
+++ randomQuoteGenerator.js
@@ -12,8 +12,9 @@ $(document).ready(function() {
 
         var url = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
 
-        //Make an AJAX Request
-        $.ajax({
+        //Make an AJAX Request - Old version (did not work because of redundant json and url format indications)
+
+        /*$.ajax({
             url: url,
             json: "json",
             dataType: "json",
@@ -32,7 +33,19 @@ $(document).ready(function() {
                     $("#author").html("- " + "Anonymous")
                 }
             }
-        });
+        });*/
+
+        //new JSON request
+
+        $get.JSON(url, function(res){
+            var newQuote = res.quoteText;
+            var newAuthor = res.quoteAuthor;
+            if(newAuthor) {
+                $("#author").html('"' + newAuthor + '"')
+            } else {
+                $("#author").html("Unknown")
+            }
+        })
     };
 
     
