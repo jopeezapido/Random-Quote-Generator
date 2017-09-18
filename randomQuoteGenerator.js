94f19c1ace8cc7e78e7fef1141128d87a5ae7196
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-18 12:14:03 +0800

    replace http with https on var url

--- randomQuoteGenerator.js
+++ randomQuoteGenerator.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
         //prepend cors data to solve cross browser issues
 
-        var url = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
+        var url = "https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
 
         //Make an AJAX Request - Old version (did not work because of redundant json and url format indications)
 
@@ -48,9 +48,7 @@ $(document).ready(function() {
         })
     };
 
-    
-    $("#quote-btn").on("click", function(){ loadData()
-    });
+    $("#quote-btn").on("click", function(){ loadData() });
 
     $("#twt-btn").on("click", function() {
         var myWindow = window.open("https://twitter.com/intent/tweet?text=" + newQuote + "--" + newAuthor, "myWindow" )
